9dffd598b9a48820e30231478866aec85b8c6e5a
JayantSravan <jayant.sravan@gmail.com>
2022-04-27 19:30:05 -0400

    Switched transformer from OpenCV to ffmpeg for speed up

--- transformer/VideoLoader/VideoLoader.py
+++ transformer/VideoLoader/VideoLoader.py
@@ -1,59 +1,34 @@
 import numpy as np
 import cv2
 import os
+from multiprocessing import Pool
+import ffmpeg
 
 class VideoTransformer(object):
-    def __init__(self, fps = 10, resolution = '1080p'):
+    def __init__(self, fps = 10, resolution = [512,512]):
         self.fps = fps
         self.resolution = resolution
 
     def _getVideo(self, filename):
-        cap = cv2.VideoCapture(filename)
-        fps = int(cap.get(5))
-        if fps < self.fps:
-            print(f"Video captured at lower frame rate than the requested fps. Video FPS - {fps}. Requested FPS - {self.fps}")
-        return cap
-    
-    # def _resize(self, image):
-    #     resized = cv2.resize(image, (1560, 1170), interpolation = cv2.INTER_AREA)
-    #     return resized
+        video_stream = ffmpeg.input(filename)
+        return video_stream
 
     def _getFrames(self, videoCapture, fps, ref_time=-1, secs_in_past = -1, secs_in_future=-1):
         
-        count = 1
-        if ref_time == -1:
-            end_time = 10000000
-            start_time = 0
-        else:
-            start_time = (ref_time - secs_in_past) * 1000
-            end_time = (ref_time + secs_in_future) * 1000
+        if ref_time != -1 and secs_in_past != -1 and secs_in_future != -1:
+            videoCapture = videoCapture.trim(start = ref_time - secs_in_past, duration = secs_in_past + secs_in_future).filter('setpts', 'PTS-STARTPTS')
         
-        
-        success = True
-        interval = 1000.0/fps
-
-        frames = []
-        
-        while success and start_time + count<end_time:
-            videoCapture.set(cv2.CAP_PROP_POS_MSEC,(start_time + count*interval))
-            count = count + 1
-            success,image = videoCapture.read()
-            
-            imageInNumpy = np.array(image)
-            
-            if imageInNumpy.shape != (1560, 1170, 3):
-                continue
-            
-            frames.append(imageInNumpy)
-            
-        if not success:
-            # throw an exception
-            pass
-        
-        return frames
+        videoCapture = videoCapture.filter('fps', fps = fps, round = 'up').filter('scale', w = self.resolution[0], h = self.resolution[1])
+        return videoCapture
 
     def _convertToNumpy(self, frames):
-        return np.array(frames)
+        out, err = (
+            frames
+            .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+            .run(capture_stdout=True)
+        )
+        video = np.frombuffer(out, np.uint8).reshape([-1, self.resolution[0], self.resolution[1], 3])[1:,:,:,:]
+        return video
 
     def transform(self, filename, ref_time=-1, secs_in_past = -1, secs_in_future=-1):
         video = self._getVideo(filename)
@@ -63,14 +38,23 @@ class VideoTransformer(object):
 
     def scrape_all_data(self, path):
         directories = [f for f in os.listdir(path)]
+        file_list = []
         for directory in directories:
             dir_path = os.path.join(path, directory)
             video_files = [f for f in os.listdir(dir_path) if f.endswith('.mp4')]
             result_dict = {}
-            for video_file in video_files:
-                output = self.transform(dir_path + '/' + video_file)
-                name = video_file.split('.')[0]
-                result_dict[name] = output
+            video_file = video_files[0]
+            file_list.append(dir_path + '/' + video_file)
+        
+        pool = Pool(os.cpu_count())
+        results = pool.map(self.transform, file_list)
+        
+        result_dict = {}
+        for idx, file in enumerate(file_list):
+            output = results[idx]
+            name = file_list[idx].split('/')[-1].split('.')[0]
+            result_dict[name] = output
+
         return result_dict
 
 videoTransformer = VideoTransformer()
