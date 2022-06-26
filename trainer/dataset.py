c59b554ead02d64b80fbd4b5598110c562dc59dd
Aishwarya Jadhav <me.aishwaryajadhav@gmail.com>
2022-06-25 19:19:50 -0700

    Trainer code cleanup

--- trainer/dataset.py
+++ trainer/dataset.py
@@ -8,7 +8,51 @@ from config import *
 # import ffmpeg
 import os
 import os.path as osp
+from utils import *
 
+# For ConvLSTM model (or other sequence-based models requiring a sequences of frames as a single training example).
+class VideoDataset(Dataset):
+    def __init__(self, files, transforms, seq_len, base_path):
+        self.transforms = transforms
+        self.files = files
+        self.seq_len = seq_len
+        self.base_path = base_path
+        X = []
+        y = []
+        for f in files:
+            df = pd.read_csv(f)
+            df_processed = preprocess_labels(df) # assign 1 sec forward labels
+
+            # Generate training sequences
+            for i in range(len(df_processed)-self.seq_len):
+                X.append(df_processed['frames'][i:i+seq_len].to_numpy())
+                y.append(df_processed['labels'][i+seq_len-1])
+        
+        self.X = np.stack(X, axis = 0)
+        self.y = np.array(y)
+        
+        
+    def __len__(self):
+        return len(self.y)
+    
+    def __getitem__(self, idx):
+        seq_filename = self.X[idx]
+        video = torch.FloatTensor(self.seq_len, CHANNELS, HEIGHT, WIDTH)
+        for e,filename in enumerate(seq_filename):
+            try:
+                frame = np.load(osp.join(self.base_path,filename), allow_pickle=True)
+                frame = (frame - frame.min())/(frame.max() - frame.min())
+                frame = self.transforms(frame)
+
+            except Exception as ex:
+                print(ex)
+                frame = torch.zeros((CHANNELS, HEIGHT, WIDTH))
+
+            video[e,:,:,:] = frame
+          
+        return video, torch.LongTensor(self.y[idx])
+        
+# For CNN model
 class FrameDataset(Dataset):
     def __init__(self, x, y, transforms, base_path):
         self.transforms = transforms
--- trainer/utils.py
+++ trainer/utils.py
@@ -2,6 +2,35 @@ import os
 import cv2
 import pandas as pd
 from config import *
+from random import random
+import os.path as osp
+import torch
+
+# df is pandas dataframe of the form: frame_path, direction, timestamp
+# direction is the current direction at the timestamp of the frame.
+# We use the following function to create label: direction 1 sec ahead 
+def preprocess_labels(df):
+    label_indices = df['timestamp'].searchsorted(df['timestamp']+1000)
+    viable_indices = label_indices[label_indices!=len(df)]
+    df_new = df[label_indices != len(df)].reset_index(drop = True)
+    df_new['labels'] = df['directions'][viable_indices].reset_index(drop = True)
+    return df_new
+    
+def make_tt_split(data_folder):
+    X = []
+    y = []
+    files = []
+    for filename in os.listdir(data_folder):
+        if(filename[-3:]=="csv"):
+            files.append(osp.join(data_folder,filename))
+    
+    random.shuffle(files)
+    
+    ts = int(len(files) * 0.25)
+    test_files = files[:ts]
+    train_files = files[ts:]
+    print("Test files ",test_files)
+    return train_files, test_files
 
 def label_map(lab):
     if(lab == 0):
@@ -26,13 +55,14 @@ def get_all_files_from_dir(directory, vids = False):
         print(e)
 
 
-def save(path, model, index, optim = False):
-    if not os.path.exists(MODELS_PATHS+path):
-        os.mkdir(MODELS_PATHS+path)
+def save(config, model, index, acc, optim = False):
+    save_path = os.path.join(config['global']['root_dir'],config['trainer']['model_save_path'], str(config['global']['iteration']))
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
     if(optim):
-        torch.save(model.state_dict(), MODELS_PATHS+path+'/optimizer_params_{:08d}.pth'.format(index))
+        torch.save(model.state_dict(), save_path+'/{}_optimizer_params_epoch_{:08d}_acc_{}.pth'.format(config['trainer']['model']['name'], index, acc))
     else:
-        torch.save(model.state_dict(), MODELS_PATHS+'/attempt_8_frames_resnet18_new_data_diff_split'+'/model_params_{:08d}.pth'.format(index))
+        torch.save(model.state_dict(), save_path+'/attempt_8_frames_resnet18_new_data_diff_split'+'/{}_model_params_epoch_{:08d}_acc_{}.pth'.format(config['trainer']['model']['name'], index, acc))
 
 
 def prep_video_test(filename):
