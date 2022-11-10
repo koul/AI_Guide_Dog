2a64fa7534bfd03f72f6471453630f59f8fe0c75
Aishwarya Narendra Jadhav <anjadhav@andrew.cmu.edu>
2022-11-10 18:02:06 -0500

    Wandb Config

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pickle
 import pdb
 import warnings
 warnings.filterwarnings("ignore")
+
 '''
 Input: a path to folder of subfolders. Each subfolder will have a CSV and MP4 file
 OUTPUT: N/A - data is dumped to a folder
@@ -61,6 +62,14 @@ TODO: full pipeline
 if __name__ == "__main__":
     config_dict = load_config()
 
+    if(config_dict['global']['enable_wandb']):
+        import wandb
+        wandb.init(name=config_dict['global']['iteration'], 
+          project="AIGD", 
+          notes=config_dict['global']['description']) 
+        #  , config=config_dict)
+    else:
+        wandb = None
 
     # avoid running transform if .nz has already been generated
     if (config_dict['global']['enable_preprocessing'] == True):
@@ -78,8 +87,11 @@ if __name__ == "__main__":
 
     if (config_dict['transformer']['enable_benchmark_test'] == True):
         test_videos = dict(np.load(config_dict['transformer']['test_save_file'] + '_video.npz', allow_pickle=True))
-    with open(config_dict['transformer']['test_save_file'] + '_sensor.pickle', 'rb') as handle:
-        test_sensor = pickle.load(handle)
+        with open(config_dict['transformer']['test_save_file'] + '_sensor.pickle', 'rb') as handle:
+            test_sensor = pickle.load(handle)
+    else:
+        test_videos = None
+        test_sensor = None
 
     # need video and sensor data separately
     with open(config_dict['transformer']['data_save_file'] + '_sensor.pickle', 'rb') as handle:
@@ -96,30 +108,28 @@ if __name__ == "__main__":
 
     val_transforms = transforms.Compose([transforms.ToTensor()])
 
-    # following functions returns a list of file paths (relative paths to video csvs) for train and test sets
-
-    # if(config_dict['data']['TEST_FILES'] is not None):
-    #     test_files = config_dict['data']['TEST_FILES']
-    #     test_files = [t.strip() for t in test_files.split(',')]
-    #     train_files = []
-    #     for f in list(df_videos.keys()):
-    #         if (f not in test_files):
-    #             train_files.append(f)
-    # else:
+    # following functions returns a list of file paths (relative paths to video csvs) for train and val sets
     train_files, val_files = make_tt_split(list(df_videos.keys()),config_dict['global']['seed'])
     
     print("Train Files:", train_files)
     print("Val Files:", val_files)
     
-    trainer = Trainer(config_dict, train_transforms, val_transforms, train_files, val_files, df_videos, df_sensor, test_videos,test_sensor)
+    trainer = Trainer(config_dict, train_transforms, val_transforms, train_files, val_files, df_videos, df_sensor, test_videos,test_sensor, wandb=wandb)
+    
     trainer.save(0, -1)
     
     epochs = config_dict['trainer']['epochs']
-
+    
     for epoch in range(epochs):
         train_actual, train_predictions = trainer.train(epoch)
         acc, val_actual, val_predictions = trainer.validate()
-        display_classification_report(train_actual, train_predictions, val_actual, val_predictions)
+        val_precision, val_recall, val_f1, _ = display_classification_report(train_actual, train_predictions, val_actual, val_predictions)
+        
+        if(config_dict['global']['enable_wandb']):
+            wandb.log({"Val Precision 0": val_precision[0], "Val Precision 1": val_precision[1], "Val Precision 2": val_precision[2]})
+            wandb.log({"Val Recall 0": val_recall[0], "Val Recall 1": val_recall[1], "Val Recall 2": val_recall[2]})
+            wandb.log({"Val F1 0": val_f1[0], "Val F1 1": val_f1[1], "Val F1 2": val_f1[2]})
+
         trainer.save(acc, epoch)
 
     print("Completed Training!!")
@@ -127,6 +137,11 @@ if __name__ == "__main__":
     if (config_dict['transformer']['enable_benchmark_test'] == True):
         print("Starting benchmark testing!!")
         acc, test_actual, test_predictions = trainer.test()
-        display_test_classification_report(test_actual, test_predictions)
+        test_precision, test_recall, test_f1, _ = display_test_classification_report(test_actual, test_predictions)
+        if(config_dict['global']['enable_wandb']):
+            wandb.log({"Test Precision 0": test_precision[0], "Test Precision 1": test_precision[1], "Test Precision 2": test_precision[2]})
+            wandb.log({"Test Recall 0": test_recall[0], "Test Recall 1": test_recall[1], "Test Recall 2": test_recall[2]})
+            wandb.log({"Test F1 0": test_f1[0], "Test F1 1": test_f1[1], "Test F1 2": test_f1[2]})
+
     
     print("Done!")
