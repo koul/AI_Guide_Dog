a747560a496f3218fe5c08e759b05f69df732a1c
Abhishree Shetty <abhishreeshetty48@gmail.com>
2022-11-10 01:21:07 -0500

    merged from default branch

--- trainer/Trainer.py
+++ trainer/Trainer.py
@@ -5,6 +5,9 @@ from trainer.models import *
 from tqdm import tqdm
 from utils import *
 
+from AI_Guide_Dog.trainer.dataset import SensorDataset
+
+
 class Trainer:
     # initialize a new trainer
     def __init__(self, config_dict, train_transforms, val_transforms, train_files, val_files, df_videos, df_sensor,
@@ -19,82 +22,119 @@ class Trainer:
         self.seq_len = config_dict['data']['SEQUENCE_LENGTH']
         self.epochs = config_dict['trainer']['epochs']
 
+        self.model_name = config_dict['trainer']['model']['name']
+        self.data_type = config_dict['trainer']['data_type']
+        model_config = config_dict['trainer']['model']
+
+
 
         if (config_dict['global']['enable_intent']):
             self.train_dataset = IntentVideoDataset(df_videos, df_sensor, train_files, transforms=train_transforms,
                                                     seq_len=self.seq_len, config_dict=self.config)
             self.val_dataset = IntentVideoDataset(df_videos, df_sensor, val_files, transforms=val_transforms,
                                                   seq_len=self.seq_len, config_dict=self.config, test='validation')
-        else:
-            self.train_dataset = VideoDataset(df_videos, df_sensor, train_files, transforms=train_transforms,
-                                              seq_len=self.seq_len, config_dict=self.config)
-            self.val_dataset = VideoDataset(df_videos, df_sensor, val_files, transforms=val_transforms,
-                                            seq_len=self.seq_len, config_dict=self.config)
+        else: # PS: multiple and enable_intent are complimentary
+            if self.data_type == "multimodal":  # If multi_modal training
+                sensor_attr_list = model_config['sensor_attr_list']
+                dense_frame_len = model_config['dense_frame_input_dim']
+                self.train_dataset = SensorVideoDataset(df_videos, df_sensor, train_files, self.model_name,
+                                                        transforms=train_transforms,
+                                                        seq_len=self.seq_len, dense_frame_len=dense_frame_len,
+                                                        sensor_attr_list=sensor_attr_list, config_dict=self.config)
+                self.val_dataset = SensorVideoDataset(df_videos, df_sensor, val_files, self.model_name,
+                                                      transforms=val_transforms,
+                                                      seq_len=self.seq_len, dense_frame_len=dense_frame_len,
+                                                      sensor_attr_list=sensor_attr_list, config_dict=self.config)
+            elif self.data_type == 'sensor':
+                sensor_attr_list = model_config['sensor_attr_list']
+                self.train_dataset = SensorDataset(df_videos, df_sensor, train_files, self.seq_len,
+                                                   sensor_attr_list=sensor_attr_list, config_dict=self.config)
+                self.val_dataset = SensorDataset(df_videos, df_sensor, val_files, self.seq_len,
+                                                 sensor_attr_list=sensor_attr_list, config_dict=self.config)
+                self.test_dataset = SensorDataset(df_videos, df_sensor, val_files, self.seq_len,
+                                                 sensor_attr_list=sensor_attr_list, config_dict=self.config)
+
+            else:
+                self.train_dataset = VideoDataset(df_videos, df_sensor, train_files, transforms=train_transforms,
+                                                  seq_len=self.seq_len, config_dict=self.config)
+                self.val_dataset = VideoDataset(df_videos, df_sensor, val_files, transforms=val_transforms,
+                                                seq_len=self.seq_len, config_dict=self.config)
+                if config_dict['transformer']['enable_benchmark_test'] and test_videos is not None:
+                    self.test_dataset = VideoDataset(test_videos, test_sensor, list(test_videos.keys()), transforms=val_transforms,
+                                                    seq_len=self.seq_len, config_dict=self.config)
 
 
-        self.model_name = config_dict['trainer']['model']['name']
 
-
-        if config_dict['trainer']['model']['name'] == "LSTM_Multimodal": #If multi_modal training
-            self.train_dataset = SensorVideoDataset(df_videos, df_sensor, train_files, self.model_name, transforms=train_transforms,
-                                                           seq_len = self.seq_len, dense_frame_len = config_dict['trainer']['model']['dense_frame_input_dim'],
-                                                           sensor_attr_list = config_dict['trainer']['model']['sensor_attr_list'], config_dict=self.config)
-        else:
-            self.train_dataset = VideoDataset(df_videos, df_sensor, train_files, transforms=train_transforms,
-                                              seq_len=self.seq_len, config_dict=self.config)
-
-
-      
-        sampler = sampler_(self.train_dataset.y, config_dict['trainer']['num_classes'])     
-        train_args = dict(batch_size=config_dict['trainer']['BATCH'], sampler = sampler, num_workers=2, pin_memory=True, drop_last=False) if self.cuda else dict(batch_size=config_dict['trainer']['BATCH'], sampler = sampler, drop_last=False)
+        sampler = sampler_(self.train_dataset.y, config_dict['trainer']['num_classes'])
+        train_args = dict(batch_size=config_dict['trainer']['BATCH'], sampler=sampler, num_workers=2, pin_memory=True,
+                          drop_last=False) if self.cuda else dict(batch_size=config_dict['trainer']['BATCH'],
+                                                                  sampler=sampler, drop_last=False)
         self.train_loader = DataLoader(self.train_dataset, **train_args)
-
-        if self.model_name == "LSTM_Multimodal": #If multi_modal training
-            self.val_dataset = SensorVideoDataset(df_videos, df_sensor, test_files, self.model_name, transforms=val_transforms,
-                                                           seq_len = self.seq_len, dense_frame_len = config_dict['trainer']['model']['dense_frame_input_dim'],
-                                                           sensor_attr_list = config_dict['trainer']['model']['sensor_attr_list'], config_dict=self.config)
-        else:
-            self.val_dataset = VideoDataset(df_videos, df_sensor, test_files, transforms=val_transforms,
-                                            seq_len=self.seq_len, config_dict=self.config)
-
 
         val_args = dict(shuffle=False, batch_size=config_dict['trainer']['BATCH'], num_workers=2, pin_memory=True, drop_last=False) if self.cuda else dict(shuffle=False, batch_size=config_dict['trainer']['BATCH'], drop_last=False)
         self.val_loader = DataLoader(self.val_dataset, **val_args)
 
         self.epochs = config_dict['trainer']['epochs']
-        hidden_dim = [int(k.strip()) for k in config_dict['trainer']['model']['convlstm_hidden'].split(',')]
 
-        channels = config_dict['data']['CHANNELS']
-        if (config_dict['global']['enable_intent']):
-            channels = channels + 1
 
         #please note that channel need to be 3 for LSTM_Multimodal
 
         if self.model_name == "ConvLSTM":
+
+            hidden_dim = [int(k.strip()) for k in config_dict['trainer']['model']['convlstm_hidden'].split(',')]
+
+            channels = config_dict['data']['CHANNELS']
+            if (config_dict['global']['enable_intent']):
+                channels = channels + 1
+
             self.model = ConvLSTMModel(channels, hidden_dim, (3,3),
                                        config_dict['trainer']['model']['num_conv_lstm_layers'], config_dict['data']['HEIGHT'],
                                        config_dict['data']['WIDTH'], True)
+
         elif self.model_name == "LSTM_Multimodal":
             self.model = LSTMModel(input_dim = config_dict['trainer']['model']['dense_frame_input_dim'] + len(config_dict['trainer']['model']['sensor_attr_list']),
                                    layer_dim = config_dict['trainer']['model']['num_lstm_layers'], hidden_dim = config_dict['trainer']['model']['lstm_hidden'],
                                    num_classes = 3)
+            elif self.model_name == "bert":
+            self.model = Bert(self.device,
+                              github_id=config_dict['wandb']['github_id'],
+                              num_attr=len(model_config['sensor_attr_list']),
+                              hidden_dim=self.hidden_dim,
+                              data_type=self.data_type,
+                              layer_num=self.layer_num
+                              )
         else:
             print("Error parsing model name, please reverify model details in config.yaml")
 
 
+
         if config_dict['transformer']['enable_benchmark_test'] and test_videos is not None:
-            if(config_dict['global']['enable_intent']):
-                self.test_dataset = IntentVideoDataset(test_videos, test_sensor, list(test_videos.keys()), transforms=val_transforms, seq_len = self.seq_len, config_dict=self.config, test= 'benchmark_test')
+            if (config_dict['global']['enable_intent']):
+                self.test_dataset = IntentVideoDataset(test_videos, test_sensor, list(test_videos.keys()),
+                                                       transforms=val_transforms, seq_len=self.seq_len,
+                                                       config_dict=self.config, test='benchmark_test')
+            elif self.data_type == "multimodal":
+                sensor_attr_list = model_config['sensor_attr_list']
+                dense_frame_len = model_config['dense_frame_input_dim']
+                self.test_dataset = SensorVideoDataset(test_videos, test_sensor, list(test_videos.keys()),
+                                                 transforms=val_transforms,
+                                                 seq_len=self.seq_len, config_dict=self.config)
+            elif self.data_type == 'sensor':
+                sensor_attr_list = model_config['sensor_attr_list']
+                self.train_dataset = SensorDataset(df_videos, df_sensor, train_files, self.seq_len,
+                                                   sensor_attr_list=sensor_attr_list,
+                                                   seq_len=self.seq_len, config_dict=self.config)
+
             else:
-                self.test_dataset = VideoDataset(test_videos, test_sensor, list(test_videos.keys()), transforms=val_transforms,
-                                            seq_len=self.seq_len, config_dict=self.config)
+                self.train_dataset = VideoDataset(test_videos, test_sensor, list(test_videos.keys()),
+                                                  transforms=val_transforms,
+                                                  seq_len=self.seq_len, config_dict=self.config)
 
             test_args = dict(shuffle=False, batch_size=config_dict['trainer']['BATCH'], num_workers=2, pin_memory=True,
-                            drop_last=False) if self.cuda else dict(shuffle=False,
-                                                                    batch_size=config_dict['trainer']['BATCH'],
-                                                                    drop_last=False)
+                             drop_last=False) if self.cuda else dict(shuffle=False,
+                                                                     batch_size=config_dict['trainer']['BATCH'],
+                                                                     drop_last=False)
             self.test_loader = DataLoader(self.test_dataset, **test_args)
-
 
 
 
