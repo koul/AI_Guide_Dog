c5468c2ffe7750107ec244bb60669aa4bf7cad01
urvashipriyam <upkumar@andrew.cmu.edu>
2022-11-09 22:38:40 -0500

    Add video transformer to videotransform files

--- transformer/VideoTransformer.py
+++ transformer/VideoTransformer.py
@@ -2,23 +2,32 @@ import numpy as np
 import os
 from multiprocessing import Pool
 import ffmpeg
+from vidaug import augmentors as va
+
 
 class VideoTransformer(object):
-    def __init__(self, fps = 10, resolution = [512,512], channels = 3):
+
+    def __init__(self, fps=10, resolution=[512, 512], channels=3):
         self.fps = fps
         self.resolution = resolution
-        self.channels =channels
+        self.channels = channels
+        self.va_transforms = va.Sequential([
+            va.RandomCrop(size=(240, 180)),
+            va.RandomRotate(degrees=10),
+        ])
 
     def _getVideo(self, filename):
-        video_stream = ffmpeg.input(filename)
+        video_stream = self.va_transforms(ffmpeg.input(filename))
         return video_stream
 
-    def _getFrames(self, videoCapture, fps, ref_time=-1, secs_in_past = -1, secs_in_future=-1):
-        
+    def _getFrames(self, videoCapture, fps, ref_time=-1, secs_in_past=-1, secs_in_future=-1):
+
         if ref_time != -1 and secs_in_past != -1 and secs_in_future != -1:
-            videoCapture = videoCapture.trim(start = ref_time - secs_in_past, duration = secs_in_past + secs_in_future).filter('setpts', 'PTS-STARTPTS')
-        
-        videoCapture = videoCapture.filter('fps', fps = fps, round = 'up').filter('scale', w = self.resolution[0], h = self.resolution[1])
+            videoCapture = videoCapture.trim(
+                start=ref_time - secs_in_past, duration=secs_in_past + secs_in_future).filter('setpts', 'PTS-STARTPTS')
+
+        videoCapture = videoCapture.filter('fps', fps=fps, round='up').filter(
+            'scale', w=self.resolution[0], h=self.resolution[1])
         return videoCapture
 
     def _convertToNumpy(self, frames):
@@ -28,12 +37,14 @@ class VideoTransformer(object):
             .run(capture_stdout=True)
         )
         print("Error: ", err)
-        video = np.frombuffer(out, np.uint8).reshape([-1, self.resolution[0], self.resolution[1], self.channels])[1:,:,:,:]
+        video = np.frombuffer(out, np.uint8).reshape(
+            [-1, self.resolution[0], self.resolution[1], self.channels])[1:, :, :, :]
         return video
 
-    def transform(self, filename, ref_time=-1, secs_in_past = -1, secs_in_future=-1):
+    def transform(self, filename, ref_time=-1, secs_in_past=-1, secs_in_future=-1):
         video = self._getVideo(filename)
-        frames = self._getFrames(video, self.fps, ref_time, secs_in_past, secs_in_future)
+        frames = self._getFrames(
+            video, self.fps, ref_time, secs_in_past, secs_in_future)
         numpyFrames = self._convertToNumpy(frames)
         return numpyFrames
 
@@ -41,10 +52,12 @@ class VideoTransformer(object):
         directories = [f for f in os.listdir(path)]
         file_list = []
         for directory in directories:
-            if directory.startswith("."):  # to ignore the temp files that get created like ".DS_Store"
+            # to ignore the temp files that get created like ".DS_Store"
+            if directory.startswith("."):
                 continue
             dir_path = os.path.join(path, directory)
-            video_files = [f for f in os.listdir(dir_path) if f.endswith('.mp4')]
+            video_files = [f for f in os.listdir(
+                dir_path) if f.endswith('.mp4')]
             print(video_files)
             if(len(video_files) == 0):
                 continue
