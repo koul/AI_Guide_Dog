49281b52f82bb5e40f658ebd07b347b8d5f50fe4
urvashipriyam <upkumar@andrew.cmu.edu>
2022-11-09 21:49:19 -0500

    Add vidaug augmentation

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from trainer.Trainer import Trainer
 import pickle
 import pdb
 import warnings
+from vidaug import augmentors as va
+
 warnings.filterwarnings("ignore")
 '''
 Input: a path to folder of subfolders. Each subfolder will have a CSV and MP4 file
@@ -18,6 +20,7 @@ https://www.dropbox.com/sh/o8orrxczmthtja6/AABCl_5tqbHt-DJoc1RnnjVDa?dl=0
 https://www.dropbox.com/sh/fbo4dr3wlpob3px/AADKhrnCyaGWCSDb6XoVOBMna?dl=0
 '''
 
+
 def save_data(video_data, sensor_data, filename):
     # np.savez(filename, **data)
     # np.savez(filename+'_sensor', **sensor_data)
@@ -25,16 +28,18 @@ def save_data(video_data, sensor_data, filename):
     save_dir = "/".join(save_dir[:-1])
     if os.path.exists(save_dir) == False:
         os.mkdir(save_dir)
-    np.savez(filename+'_video', **video_data)
-    with open(filename+'_sensor.pickle', 'wb') as handle:
+    np.savez(filename + '_video', **video_data)
+    with open(filename + '_sensor.pickle', 'wb') as handle:
         pickle.dump(sensor_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        
+
 def load_data(filename):
     return np.load(filename, allow_pickle=True)
 
+
 def transform(data_file_path, fps, data_save_file, resolution, channels):
-    dataTransformer = DataTransformer.DataTransformer(fps, resolution, channels)
+    dataTransformer = DataTransformer.DataTransformer(
+        fps, resolution, channels)
     video_data, sensor_data = dataTransformer.scrape_all_data(data_file_path)
     # print(video_data.keys())
     # exit()
@@ -45,7 +50,7 @@ def transform(data_file_path, fps, data_save_file, resolution, channels):
 After calling transform, train model on the dumped data
 in the folders
 '''
-#def train_model():
+# def train_model():
 
 
 def load_config():
@@ -61,42 +66,51 @@ TODO: full pipeline
 if __name__ == "__main__":
     config_dict = load_config()
 
-
     # avoid running transform if .nz has already been generated
     if (config_dict['global']['enable_preprocessing'] == True):
         transform(config_dict['transformer']['path'], config_dict['transformer']['fps'],
                   config_dict['transformer']['data_save_file'],
                   [config_dict['data']['HEIGHT'], config_dict['data']['WIDTH']],
                   config_dict['data']['CHANNELS'])
-        if (config_dict['transformer']['enable_benchmark_test'] == True): transform(
-            config_dict['transformer']['test_path'], config_dict['transformer']['fps'],
-            config_dict['transformer']['test_save_file'], [config_dict['data']['HEIGHT'], config_dict['data']['WIDTH']],
-            config_dict['data']['CHANNELS'])
+        if (config_dict['transformer']['enable_benchmark_test'] == True):
+            transform(
+                config_dict['transformer'][
+                    'test_path'], config_dict['transformer']['fps'],
+                config_dict['transformer']['test_save_file'], [
+                    config_dict['data']['HEIGHT'], config_dict['data']['WIDTH']],
+                config_dict['data']['CHANNELS'])
 
-    df_videos = dict(np.load(config_dict['transformer']['data_save_file'] + '_video.npz', allow_pickle=True))
+    df_videos = dict(np.load(config_dict['transformer'][
+                     'data_save_file'] + '_video.npz', allow_pickle=True))
     print(df_videos.keys())
 
     if (config_dict['transformer']['enable_benchmark_test'] == True):
-        test_videos = dict(np.load(config_dict['transformer']['test_save_file'] + '_video.npz', allow_pickle=True))
+        test_videos = dict(np.load(config_dict['transformer'][
+                           'test_save_file'] + '_video.npz', allow_pickle=True))
     with open(config_dict['transformer']['test_save_file'] + '_sensor.pickle', 'rb') as handle:
         test_sensor = pickle.load(handle)
 
     # need video and sensor data separately
     with open(config_dict['transformer']['data_save_file'] + '_sensor.pickle', 'rb') as handle:
         df_sensor = pickle.load(handle)
-    
+
     # pdb.set_trace()
     # print(df_sensor['sample']['direction_label']['direction'])
-    
+
     # Training setup begins
     # train_transforms = [ttf.ToTensor(), transforms.Resize((HEIGHT, WIDTH)), transforms.ColorJitter(), transforms.RandomRotation(10), transforms.GaussianBlur(3)]
     # train_transforms = transforms.Compose([transforms.ToTensor(), transforms.Resize((config_dict['data']['HEIGHT'], config_dict['data']['WIDTH']))])
 
-    train_transforms = transforms.Compose([transforms.ToTensor()])
+    # train_transforms = transforms.Compose([transforms.ToTensor()])
+    train_transforms = va.Sequential([
+        va.RandomCrop(size=(240, 180)),
+        va.RandomRotate(degrees=10),
+    ])
 
     val_transforms = transforms.Compose([transforms.ToTensor()])
 
-    # following functions returns a list of file paths (relative paths to video csvs) for train and test sets
+    # following functions returns a list of file paths (relative paths to
+    # video csvs) for train and test sets
 
     # if(config_dict['data']['TEST_FILES'] is not None):
     #     test_files = config_dict['data']['TEST_FILES']
@@ -106,20 +120,23 @@ if __name__ == "__main__":
     #         if (f not in test_files):
     #             train_files.append(f)
     # else:
-    train_files, val_files = make_tt_split(list(df_videos.keys()),config_dict['global']['seed'])
-    
+    train_files, val_files = make_tt_split(
+        list(df_videos.keys()), config_dict['global']['seed'])
+
     print("Train Files:", train_files)
     print("Val Files:", val_files)
-    
-    trainer = Trainer(config_dict, train_transforms, val_transforms, train_files, val_files, df_videos, df_sensor, test_videos,test_sensor)
+
+    trainer = Trainer(config_dict, train_transforms, val_transforms,
+                      train_files, val_files, df_videos, df_sensor, test_videos, test_sensor)
     trainer.save(0, -1)
-    
+
     epochs = config_dict['trainer']['epochs']
 
     for epoch in range(epochs):
         train_actual, train_predictions = trainer.train(epoch)
         acc, val_actual, val_predictions = trainer.validate()
-        display_classification_report(train_actual, train_predictions, val_actual, val_predictions)
+        display_classification_report(
+            train_actual, train_predictions, val_actual, val_predictions)
         trainer.save(acc, epoch)
 
     # performs final benchmarking after training
